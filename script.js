8a70fbf226d2fffd3749b2d4e1a805c40c49ee8e
Bojo <bozhidar_zahariev@hotmail.com>
2020-07-31 12:30:37 +0100

    more exercises

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ import subtract from './math_functions.js';
 // Only change code above this line
 
 subtract(7, 4);
+
+//ES6: Create a JavaScript Promise
+const makeServerRequest = new Promise((resolve, reject) => {});
+
+//ES6: Complete a Promise with resolve and reject
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+
+  if (responseFromServer) {
+    resolve('We got the data');
+  } else {
+    reject('Data not received');
+  }
+});
