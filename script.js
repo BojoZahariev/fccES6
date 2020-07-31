97e04598578813d8f5b6ad7a5168a5ae90d16a45
Bojo <bozhidar_zahariev@hotmail.com>
2020-07-31 12:39:55 +0100

    end of es6

--- script.js
+++ script.js
@@ -20,3 +20,40 @@ const makeServerRequest = new Promise((resolve, reject) => {
     reject('Data not received');
   }
 });
+
+//ES6: Handle a Fulfilled Promise with then
+
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if (responseFromServer) {
+    resolve('We got the data');
+  } else {
+    reject('Data not received');
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+//ES6: Handle a Rejected Promise with catch
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+
+  if (responseFromServer) {
+    resolve('We got the data');
+  } else {
+    reject('Data not received');
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+makeServerRequest.catch(error => {
+  console.log(error);
+});
