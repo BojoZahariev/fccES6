cfa2eef62c2ea11043e1005ab319ad2173c66e3d
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-03 10:31:30 +0100

    more exercises

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//ES6: Use getters and setters to Control Access to an Object
+class Book {
+  constructor(author) {
+    this._author = author;
+  }
+  // getter
+  get writer() {
+    return this._author;
+  }
+  // setter
+  set writer(updatedAuthor) {
+    this._author = updatedAuthor;
+  }
+}
+
 //ES6: Import a Default Export
 
 import subtract from './math_functions.js';
