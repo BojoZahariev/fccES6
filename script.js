59f7a3dfe28ac36242443eec17a01d176a54dc5e
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-05 16:56:12 +0100

    exercises

--- script.js
+++ script.js
@@ -203,3 +203,41 @@ function countOnline(obj) {
   // change code above this line
 }
 console.log(countOnline(users));
+
+//Basic Data Structures: Generate an Array of All Object Keys with Object.keys()
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  let arr = [];
+  arr = Object.keys(obj);
+  return arr;
+  // Only change code above this line
+}
+
+//Basic Data Structures: Modify an Array Stored in an Object
+
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: ['Sam', 'Kira', 'Tomo'],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  console.log(userObj.data.friends);
+  userObj.data.friends.push(friend);
+  // Only change code above this line
+  return userObj.data.friends;
+}
+
+console.log(addFriend(user, 'Pete'));
