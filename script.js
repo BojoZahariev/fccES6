6eef1cb74e4e9cf6f3f1c2cdea2a919cca93cdc2
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-11 12:11:50 +0100

    exercises

--- script.js
+++ script.js
@@ -471,4 +471,23 @@ function Dog() {
   this.numLegs = 4;
 }
 
+//Object Oriented Programming: Use a Constructor to Create Objects
+
+function Dog() {
+  this.name = 'Rupert';
+  this.color = 'brown';
+  this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
+
+//Object Oriented Programming: Extend Constructors to Receive Arguments
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4;
+}
+
+let terrier = new Dog('Chocho', 'black');
+
 dog.sayLegs();
