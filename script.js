4a6db5309d8bc1f67b54667888ec22b66d8d1d79
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-13 09:03:05 +0100

    exercices

--- script.js
+++ script.js
@@ -528,3 +528,26 @@ function Dog(name) {
 Dog.prototype.numLegs = 4;
 // Only change code above this line
 let beagle = new Dog('Snoopy');
+
+//Object Oriented Programming: Iterate Over All Properties
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog('Snoopy');
+
+let ownProps = [];
+let prototypeProps = [];
+
+// Only change code below this line
+
+for (let property in beagle) {
+  if (beagle.hasOwnProperty(property)) {
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
