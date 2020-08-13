fc28fa1b720ffea8c8a0bed63a93154dd1eb31ad
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-13 10:26:52 +0100

    exercises

--- script.js
+++ script.js
@@ -563,3 +563,19 @@ function joinDogFraternity(candidate) {
     return false;
   }
 }
+
+//Object Oriented Programming: Change the Prototype to a New Object
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  numLegs: 4,
+  eat: function() {
+    console.log('nom nom nom');
+  },
+  describe: function() {
+    console.log('My name is ' + this.name);
+  }
+};
