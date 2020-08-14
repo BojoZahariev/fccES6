e78eada8331290e8bf1177976871d5cda0f0df6c
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-14 10:14:11 +0100

    exercises

--- script.js
+++ script.js
@@ -608,3 +608,16 @@ let beagle = new Dog('Snoopy');
 
 // Only change code below this line
 Dog.prototype.isPrototypeOf(beagle);
+
+//Object Oriented Programming: Understand the Prototype Chain
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog('Snoopy');
+
+Dog.prototype.isPrototypeOf(beagle); // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
