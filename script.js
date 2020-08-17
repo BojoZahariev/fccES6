5d27c993c362ee06055d26c92708db51c21346db
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-17 15:31:36 +0100

    exercise

--- script.js
+++ script.js
@@ -664,3 +664,21 @@ Animal.prototype = {
 
 let duck = Object.create(Animal.prototype); // Change this line
 let beagle = Object.create(Animal.prototype); // Change this line
+
+//Object Oriented Programming: Set the Child's Prototype to an Instance of the Parent
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log('nom nom nom');
+  }
+};
+
+function Dog() {}
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
