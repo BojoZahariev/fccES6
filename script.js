12544727584a1de52ca7b99701d5ba2439918208
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-18 10:02:31 +0100

    exercises

--- script.js
+++ script.js
@@ -698,3 +698,24 @@ Dog.prototype.constructor = Dog;
 
 let duck = new Bird();
 let beagle = new Dog();
+
+//Object Oriented Programming: Add Methods After Inheritance
+
+function Animal() {}
+Animal.prototype.eat = function() {
+  console.log('nom nom nom');
+};
+
+function Dog() {}
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function() {
+  console.log('Woof!');
+};
+
+// Only change code above this line
+
+let beagle = new Dog();
