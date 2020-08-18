806fd12a9db68e5d97d551c876bd838d18cdc366
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-18 10:05:23 +0100

    exercises

--- script.js
+++ script.js
@@ -708,7 +708,6 @@ Animal.prototype.eat = function() {
 
 function Dog() {}
 
-// Only change code below this line
 Dog.prototype = Object.create(Animal.prototype);
 Dog.prototype.constructor = Dog;
 
@@ -716,6 +715,23 @@ Dog.prototype.bark = function() {
   console.log('Woof!');
 };
 
-// Only change code above this line
-
 let beagle = new Dog();
+
+//Object Oriented Programming: Override Inherited Methods
+
+function Bird() {}
+
+Bird.prototype.fly = function() {
+  return 'I am flying!';
+};
+
+function Penguin() {}
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+Penguin.prototype.fly = function() {
+  return 'Alas, this is a flightless bird.';
+};
+
+let penguin = new Penguin();
+console.log(penguin.fly());
