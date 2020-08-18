69bac032fc225a1ef942e3b4d45004f8cc12c721
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-18 13:41:57 +0100

    exercise

--- script.js
+++ script.js
@@ -768,3 +768,9 @@ function Bird() {
     return weight;
   };
 }
+
+//Object Oriented Programming: Understand the Immediately Invoked Function Expression (IIFE)
+
+(function() {
+  console.log('A cozy nest is ready');
+})();
