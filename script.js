56767951258bebdbb9a86e37878b1ba4f5c22a1c
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-19 16:21:20 +0100

    exercises

--- script.js
+++ script.js
@@ -925,3 +925,41 @@ function incrementer(num) {
 
   // Only change code above this line
 }
+
+//Functional Programming: Refactor Global Variables Out of Functions
+
+// The global variable
+var bookList = [
+  'The Hound of the Baskervilles',
+  'On The Electrodynamics of Moving Bodies',
+  'Philosophiæ Naturalis Principia Mathematica',
+  'Disquisitiones Arithmeticae'
+];
+
+// Change code below this line
+function add(arr, bookName) {
+  let newList = [...arr];
+  newList.push(bookName);
+  return newList;
+
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(arr, bookName) {
+  var book_index = arr.indexOf(bookName);
+  let newList = [...arr];
+  if (book_index >= 0) {
+    newList.splice(book_index, 1);
+
+    return newList;
+
+    // Change code above this line
+  }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
