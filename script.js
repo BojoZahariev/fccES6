038479c411e8a1ef687881956062b541fcc71f47
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-20 13:36:04 +0100

    exercises

--- script.js
+++ script.js
@@ -1243,3 +1243,25 @@ var filteredList = watchList
 // Only change code above this line
 
 console.log(filteredList);
+
+//Functional Programming: Implement the filter Method on a Prototype
+
+// The global variable
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  var newArray = [];
+  this.forEach(el => {
+    if (callback(el)) {
+      newArray.push(el);
+    }
+  });
+
+  // Only change code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
