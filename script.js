7c8c86fd1581c34c25999152c2bcde528279f183
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-25 22:00:52 +0100

    exercises

--- script.js
+++ script.js
@@ -1476,3 +1476,14 @@ function alphabeticalOrder(arr) {
   // Only change code above this line
 }
 alphabeticalOrder(['a', 'd', 'c', 'a', 'z', 'g']);
+
+//Functional Programming: Return a Sorted Array Without Changing the Original Array
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return [...arr].sort((a, b) => a - b);
+
+  // Only change code above this line
+}
+nonMutatingSort(globalArray);
