ca437d841151a30fc363037de7e9062c77d23182
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-25 22:06:30 +0100

    exercises

--- script.js
+++ script.js
@@ -1487,3 +1487,12 @@ function nonMutatingSort(arr) {
   // Only change code above this line
 }
 nonMutatingSort(globalArray);
+
+//Functional Programming: Split a String into an Array Using the split Method
+
+function splitify(str) {
+  // Only change code below this line
+  return str.split(/\W/);
+  // Only change code above this line
+}
+splitify('Hello World,I-am code');
