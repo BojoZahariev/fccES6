7a8a5be167688bc8a4c5fce16efeb1bb6c9509ed
Bojo <bozhidar_zahariev@hotmail.com>
2020-08-26 22:47:13 +0100

    exercise

--- script.js
+++ script.js
@@ -1496,3 +1496,13 @@ function splitify(str) {
   // Only change code above this line
 }
 splitify('Hello World,I-am code');
+
+//Functional Programming: Combine an Array into a String Using the join Method
+
+function sentensify(str) {
+  // Only change code below this line
+  return str.split(/\W/).join(' ');
+
+  // Only change code above this line
+}
+sentensify('May-the-force-be-with-you');
